1f1171ff0b7c0bddb752d379b194ec5e95d2a619
Daniel Cosme <pipecosme123@gmail.com>
2021-05-28 23:08:25 -0500

    Cleaning the record.js file

--- record.js
+++ record.js
@@ -1,35 +1,39 @@
-/*
-library used: https://github.com/TrevorSundberg/h264-mp4-encoder 
-a simple example exporting mp4 with p5js.
-record video while animation is being played.
-*/
+// SIZE OF CANVAS
 let cwidth = 960;
 let cheight = 540;
 
+// RECORD VIDEO
 let encoder;
-
 const frate = 30; // frame rate- Max:9000Frames
 const numFrames = 9000; // num of frames to record
 let recording = false;
 let recordedFrames = 0;
-
-let count = 0;
-let x =0;
+//-----------------------------------------------
 let fTriangle = 0;
-let figura;
 
-// Record audio
-let mic, recorder, soundFile, state;
+// Variables para los personajes y los escenarios
+let imgCharacter, imgCharacter1, imgCharacter2, imgCharacter3, imgScene, imgScene1, imgScene2, imgScene3;
 
+//Variables para animación
+let xMove;
+let yMove;
 
-document.getElementById("record").onclick = function() {clickRecording()};
-document.getElementById("stop").onclick = function() {stopRecording()};
+//Complemento de los botones para la funcion de grabar.
+document.getElementById("record").onclick = function () { clickRecording() };
+document.getElementById("stop").onclick = function () { stopRecording() };
 
-// Record audio
-document.getElementById("recordV").onclick= function(){recordVoice()};
-document.getElementById("stopV").onclick= function(){stopRecordVoice()};
+// Toma los datos de los botones character
+document.getElementById("character1").onclick = function () { selectedCharacter(document.getElementById("character1").value) };
+document.getElementById("character2").onclick = function () { selectedCharacter(document.getElementById("character2").value) };
+document.getElementById("character3").onclick = function () { selectedCharacter(document.getElementById("character3").value) };
 
-// make sure encoder is ready before use
+// Toma los datos de los botones scene
+document.getElementById("scene1").onclick = function () { selectedScene(document.getElementById("scene1").value) };
+document.getElementById("scene2").onclick = function () { selectedScene(document.getElementById("scene2").value) };
+document.getElementById("scene3").onclick = function () { selectedScene(document.getElementById("scene3").value) };
+//-----------------------------------------------------
+
+// Carga todos los elementos de la grabación de video.
 function preload() {
     HME.createH264MP4Encoder().then(enc => {
         encoder = enc;
@@ -41,83 +45,96 @@ function preload() {
         encoder.groupOfPictures = 10; // lower if you have fast actions.
         encoder.initialize();
     })
+
+    imgCharacter1 = loadImage("./img/hombre.svg");
+    imgCharacter2 = loadImage("./img/hombre1.svg");
+    imgCharacter3 = loadImage("./img/mujer.svg");
+    imgScene1 = loadImage("./img/paisaje1.jpg");
+    imgScene2 = loadImage("./img/paisaje2.jpg");
+    imgScene3 = loadImage("./img/paisaje3.jpg");
 }
 
-function setup() {
-    
-    var canvas = createCanvas(cwidth, cheight);
-    canvas.parent('canvas');
-    // background('rgba(0,255,0, 0.25)');
-    frameRate(frate);
-    //button = button = createButton('record')
-    //button.mousePressed(() => recording = true);
-    //loop();
-
-    // Record audio
-    mic = new p5.AudioIn();
-    mic.start();
-    recorder = new p5.SoundRecorder();
-    recorder.setInput(mic);
-    soundFile = new p5.SoundFile();
-    //mic.start();
-
-    // figura = triangle(30, 75, 58, 20, 86, 75);
-}
-
-function clickRecording(){
+function clickRecording() {
     recording = true;
 }
-function stopRecording(){
+function stopRecording() {
     recording = false;
 }
 
-// Record audio
-function recordVoice(){
-    // console.log("Record Voice");
-    state = 0;
-    recordV();
-}
-function stopRecordVoice(){
-    // console.log("Stop record Voice");
-    fTriangle = 1;
-    console.log(fTriangle);
-    // stopVoice();
+function selectedCharacter(value) {
+    imgCharacter = value;
+    console.log("The character is " + value);
 }
 
+function selectedScene(value) {
+    imgScene = value;
+    console.log("The Scene is " + value);
+}
+
+function setup() {
+    var canvas = createCanvas(cwidth, cheight);
+    canvas.parent('canvas');
+    frameRate(frate);
+
+    // xMove = width / 2;
+    yMove = cheight;
+}
 function draw() {
-    background(200,0,0);
-    ellipse(150, 100, 100, 100);
-    rectMode(CENTER);
-    translate(width / 2, height / 2);
-    translate(p5.Vector.fromAngle(millis() / 1000, 40));
-    rect(0, 0, 20, 20);
-    // figura = triangle(30, 75, 58, 20, 86, 75);
-    // background('rgba(255,255,255,0.25)');
+    background(200);
+    //background(image(imgScene1, 0, 0, cwidth, cheight));
+    ellipse(200, yMove, 24, 24);
+    // moverse aleatoriamente en el eje x
+    //xMove = xMove + random(-1, 1);
+    // mover hacia arriba a velocidad constante
     
-    if(fTriangle == 1){
-        triangle(30, 75, 58, 20, 86, 75);
-        // figura;
-        // console.log("Entro");
+    // if (xMove > cwidth) {
+    //     xMove = 0;
+    // }
+    // xMove++
+
+    // //yMove = yMove - 5;
+    // // reset al fondo
+    if (yMove == 0) {
+        yMove = cheight;
     }
-    //fTriangle++;
-    
-    /*
-    x = x + 0.1;
-    if (x >width) {
-        x = 0;
-    };
-    line(x, 0, x, height);
-    */
+    yMove--;
+
+    if (imgScene == 1) {
+        image(imgScene1, 0, 0, cwidth, cheight);
+    } else if (imgScene == 2) {
+        image(imgScene2, 0, 0, cwidth, cheight);
+    } else if (imgScene == 3) {
+        image(imgScene3, 0, 0, cwidth, cheight);
+    }
+
+    if (imgCharacter == 1) {
+        image(imgCharacter1, 0, 0, 200, 200);
+    } else if (imgCharacter == 2) {
+        image(imgCharacter2, 0, 0, 200, 200);
+    } else if (imgCharacter == 3) {
+        image(imgCharacter3, 0, 0, 200, 200);
+    }
+
+
+
+    //rectMode(CENTER);
+    //translate(width / 2, height / 2);
+    //translate(p5.Vector.fromAngle(millis() / 1000, 40));
+    //rect(0, 0, 20, 20);
+
+    if (fTriangle == 1) {
+        triangle(30, 75, 58, 20, 86, 75);
+    }
+    //--------------------------------------------------------------
+    //Grabación y guardado de video.
     // keep adding new frame
-    
     if (recording) {
         console.log('recording');
         encoder.addFrameRgba(drawingContext.getImageData(0, 0, encoder.width, encoder.height).data);
         recordedFrames++;
     }
-    
     // finalize encoding and export as mp4
-    if (recordedFrames === numFrames || (recordedFrames > 0 && recording == false))  {
+    if (recordedFrames === numFrames || (recordedFrames > 0 && recording == false)) {
         recording = false;
         recordedFrames = 0;
         console.log('recording stopped');
@@ -129,36 +146,6 @@ function draw() {
         anchor.download = encoder.outputFilename;
         anchor.click();
         encoder.delete();
-
         preload();
-    }
-}
-
-function recordV() {
-    // usar el booleano '.enabled' (permitido) para asegurarse que el micrófono haya sido habilitado por el usuario (si no grabaríamos silencio)
-    getAudioContext().resume();
-    if (state === 0 && mic.enabled) {
-        console.log("recordingVoice");
-        // indicar al grabador que grabe en el objeto p5.SoundFile, que usaremos para la reproducción
-        recorder.record(soundFile);
-
-        //alert("Voice s recording");
-        //text('Recording now! Click to stop.', 20, 20);
-        //state++;
-
-    } 
-}
-
-function stopVoice() {
-    if (state === 1) {
-        console.log("StopVoice");
-        recorder.stop(); // parar el grabador, y enviar el resultado al archivo de audio soundFile
-
-        //background(0, 255, 0);
-        //text('Recording stopped. Click to play & save', 20, 20);
-        //state++;
-        //soundFile.play(); // reproduce el sonido
-        saveSound(soundFile, 'mySound.wav'); // almacena el archivo
-        state++;
     }
 }
